9c1e42a88fdb018ed5f1b082745635cd787caf19
aakanksha717 <55994459+aakanksha717@users.noreply.github.com>
2021-08-06 18:55:43 +0530

    initial commit

---
+++ bank account.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+class A
+{
+	public:
+		char name,type;
+		int accnum,w,d;
+		float bal;
+	public:
+		void initial()
+		{
+			char name[]= "aakanksha";
+			char type[]= "savings";
+			accnum=25367819486;
+			bal=5000;
+		}
+		void print()
+		{
+			cout<<"accnum="<<"25367819486"<<endl;
+			cout<<"name="<<"Aakanksha"<<endl;
+			cout<<"type="<<"savings"<<endl;
+		}
+		void deposit()
+		{
+			cout<<"balance="<<bal<<endl;
+			cout<<"enter the amount to be deposited=";
+			cin>>d;
+			bal=bal+d;
+			cout<<"bal after deposition="<<bal<<endl;
+		}
+		void withdraw()
+		{
+			cout<<"enter the withdrawl amount=";
+			cin>>w;
+			if(w>bal)
+            cout<<"\nInsuficient balance";
+            else
+            {
+			bal=bal-w;
+		    cout<<"bal after withdrawl="<<bal<<endl;
+			cout<<"net bal="<<bal<<endl;
+		    }
+			time_t tt; 
+			struct tm * ti;
+			time (&tt); 
+			ti = localtime(&tt); 
+			cout << "\nDate and time of Withdrawl = "  << asctime(ti);
+			cout<<"Have a good day!"<<endl;
+		}
+};
+int main()
+{
+	A O;
+	O.initial();
+	O.print();
+	O.deposit();
+	O.withdraw();
+	return 0;
+}
